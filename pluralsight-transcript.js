80460f1974048574b75f0cf3c651c3cc88a0b2fc
Luke Paireepinart <lukepdev@live.com>
2017-03-16 23:27:54 -0500

    - Added code to pluralsight-transcript that allows getting active module and current time in video - Updated show-button to parse the current course and make a popup window that shows the JSON file - TODO: write JSON parser for modules - TODO: hook up pluralsight-transcript to execute on the page - TODO: update show-button.js to use the player URL instead of the library URL

--- pluralsight-transcript.js
+++ pluralsight-transcript.js
@@ -5,3 +5,38 @@ $(document).ready(function() {
   alert($('#course-title').html());
 });
 */
+
+
+
+function hasClass(item, className) {
+    for (var i = 0; i < item.classList.length; i++) {
+        if (item.classList[i] == className) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+var activeModule = -1;
+var activeClip = -1;
+// First find all <section> items under #modules
+var children = document.getElementsByClassName("modules")[0].children;
+
+for (var i = 0; i < children.length; i++) {
+
+    var currentChild = children[i];
+    // Class "open"
+    var foundActive = hasClass(currentChild, "open");
+    if (foundActive) {
+        var clipList = currentChild.children[1].children;
+        for (var j = 0; j < clipList.length; j++) {
+            if (hasClass(clipList[j], "selected")) {
+                activeModule = i;
+                activeClip = j;
+            }
+        }
+    }
+}
+
+var currentTime = document.getElementsByTagName('video')[0].currentTime
--- show-button.js
+++ show-button.js
@@ -1,22 +1,31 @@
 
 const CSS = "body { border: 20px solid red; }";
-
+var active_course = "";
 function showTranscript(tab) {
-  console.log("Show Transcript");
+    console.log("Show Transcript");
+    console.log("Tab: " + tab);
+    console.log("Course: " + active_course);
   // TODO: parse out course name, use in this URL:
   // "https://app.pluralsight.com/learner/courses/" + courseName + "/transcript"
-  browser.windows.create(
+  /*browser.windows.create(
     {
       type : "popup",
       url : "transcript.html"
-    });
+    });*/
+    browser.windows.create(
+        {
+            type: "popup",
+            url: "https://app.pluralsight.com/learner/courses/" + active_course + "/transcript"
+        });
 }
 //https://app.pluralsight.com/player?course=getting-started-opencv-net&author=kobi-hikri&name=getting-started-opencv-net-m2&clip=0&mode=live
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  //if (tab.url.match(/^https\:\/\/app\.pluralsight\.com\/player/)) {
-  if (tab.url.match(/^https\:\/\/app\.pluralsight\.com\/library/courses/)) {
-
-    browser.pageAction.show(tab.id);
+  //if (tab.url.match(/^https\:\/\/app\.pluralsight\.com\/player/?.*course=[^&]*&)) {
+    regex = /^https\:\/\/app\.pluralsight\.com\/library\/courses\/([^\/]*)/ig;
+    if (tab.url.match(regex)) {
+        match = regex.exec(tab.url);
+        browser.pageAction.show(tab.id);
+        active_course = match[1];
   }
 });
 
