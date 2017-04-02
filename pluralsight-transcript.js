7da60752106ce7e3f72f624ce43f708ec9d690d7
Luke Paireepinart <lukepdev@live.com>
2017-04-02 15:41:21 -0500

    - Implemented proper regex matching on live Pluralsight videos - Track whether a specific tab has had a popup launched and add it to a collection so we don't re-make the popup (TODO: focus existing if exists?) - Implemented live messaging between injected content script and background script with the current module, clip and time every 100 ms

--- pluralsight-transcript.js
+++ pluralsight-transcript.js
@@ -1,12 +1,21 @@
-/*document.body.style.border = "5px solid red";
-pageAction.show();
-$(document).ready(function() {
-  //$('#course-title').html($('#course-title'.html() + "F U"));//<button id="button-notes" class="active side-menu-tab-button">Notes</button>
-  alert($('#course-title').html());
+// http://app.pluralsight.com/player.html?course=ionic2-angular2-typescript-mobile-apps&author=steve-michelotti&name=ionic2-angular2-typescript-mobile-apps-m0&clip=0&mode=live
+
+var myPort = browser.runtime.connect({name:"port-from-cs"});/*
+myPort.postMessage({greeting: "hello from content script"});
+
+myPort.onMessage.addListener(function(m) {
+  console.log("In content script, received message from background script: ");
+  console.log(m.greeting);
 });
+document.body.addEventListener("click", function() {
+  myPort.postMessage({greeting: "they clicked the page!"});
+});
+
 */
 
-
+var currentModule = -1;
+var currentClip = -1;
+var currentTime = -1;
 
 function hasClass(item, className) {
     for (var i = 0; i < item.classList.length; i++) {
@@ -17,26 +26,65 @@ function hasClass(item, className) {
     return false;
 }
 
+function dataChanged() {
+    myPort.postMessage(
+    {
+        module: currentModule,
+        clip: currentClip,
+        time: currentTime
+    });
+}
 
-var currentModule = -1;
-var currentClip = -1;
-// First find all <section> items under #modules
-var children = document.getElementsByClassName("modules")[0].children;
+function updateData()
+{
+    var data_changed = false;
+    // First find all <section> items under #modules
+    var children = document.getElementsByClassName("modules")[0].children;
+    // Then check all children to see what clip is selected.
+    for (var i = 0; i < children.length; i++) {
 
-for (var i = 0; i < children.length; i++) {
-
-    var currentChild = children[i];
-    // Class "open"
-    var foundActive = hasClass(currentChild, "open");
-    if (foundActive) {
+        var currentChild = children[i];
         var clipList = currentChild.children[1].children;
         for (var j = 0; j < clipList.length; j++) {
             if (hasClass(clipList[j], "selected")) {
+                if (currentModule != i || currentClip != j)
+                {
+                    console.log("Module and clip changed.");
+                    console.log(currentModule);
+                    console.log(i);
+                    console.log(currentClip);
+                    console.log(j);
+                    data_changed = true;
+                }
                 currentModule = i;
                 currentClip = j;
+                break;
             }
         }
     }
+    var newTime = document.getElementsByTagName('video')[0].currentTime;
+    if (currentTime != newTime)
+    {
+        data_changed = true;
+    }
+    currentTime = newTime;
+    if (data_changed)
+    {
+        dataChanged();
+    }
+    // Set up a recursive loop to constantly check to see if any of our watched values
+    // have changed since the last time, and if so, notify the background script.
+    window.setTimeout(updateData, 100);
 }
+// Invoke updateData immediately upon definition so we don't have any lag time
+// from the first time the button is clicked.
+updateData();
 
-var currentTime = document.getElementsByTagName('video')[0].currentTime
+/*
+(function() {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'alert("hi")';
+    document.body.appendChild(script);
+})();
+*/
--- show-button.js
+++ show-button.js
@@ -1,32 +1,74 @@
 
 const CSS = "body { border: 20px solid red; }";
-var active_course = "";
+
+// Production Regex
+var playerRegex = /^https?\:\/\/app\.pluralsight\.com\/player\?.*course=([^&]*)(&|$)/ig;
+// Local Mock Regex (IIS can't easily serve static files with no extension).
+//var playerRegex = /^https?\:\/\/app\.pluralsight\.com\/player.html\?.*course=([^&]*)(&|$)/ig;
+
+var active_callbacks = [];
+
+/*
+  Define callback function that will lookup the active course popup in our list and
+  Update the DOM to highlight the current line of the transcript.
+  Also define a second callback that will call updateTranscriptTIme
+*/
+function updateTranscriptTime(m) {
+    console.log("In background script, received message from content script");
+    console.log("MODULE: " + m.module);
+    console.log("CLIP: " + m.clip);
+    console.log("TIME: " + m.time);
+}
+
+function onCreated(windowInfo) {
+  console.log(`Created window: ${windowInfo.id}`);
+}
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+/* Define callback function for onclick of Page Action that will launch transcript
+   popup and inject script into player page to update us with the current player time
+*/
 function showTranscript(tab) {
+    active_course = playerRegex.exec(tab.url)[1];
     console.log("Show Transcript");
     console.log("Tab: " + tab);
     console.log("Course: " + active_course);
-  // TODO: parse out course name, use in this URL:
-  // "https://app.pluralsight.com/learner/courses/" + courseName + "/transcript"
-  /*browser.windows.create(
+    // Returns 404 if can't find course.
+    // What if can't find transcript but course exists?
+
+    // Production Transcript URL
+    var transcriptJSONurl = "https://app.pluralsight.com/learner/courses/" + active_course + "/transcript"
+    // Local Mock Transcript URL (IIS can't easily serve static files with no extension).
+    //var transcriptJSONurl = "https://app.pluralsight.com/learner/courses/" + active_course + "/transcript.html"
+    var transcriptURL = "transcript.html";//browser.extension.getURL("transcript.html");
+    transcriptURL += "?course=" + active_course;
+    var creating = browser.windows.create(
     {
       type : "popup",
-      url : "transcript.html"
-    });*/
-    browser.windows.create(
-        {
-            type: "popup",
-            url: "https://app.pluralsight.com/learner/courses/" + active_course + "/transcript"
-        });
-}
-//https://app.pluralsight.com/player?course=getting-started-opencv-net&author=kobi-hikri&name=getting-started-opencv-net-m2&clip=0&mode=live
-browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  //if (tab.url.match(/^https\:\/\/app\.pluralsight\.com\/player/?.*course=[^&]*&)) {
-    regex = /^https\:\/\/app\.pluralsight\.com\/library\/courses\/([^\/]*)/ig;
-    if (tab.url.match(regex)) {
-        match = regex.exec(tab.url);
-        browser.pageAction.show(tab.id);
-        active_course = match[1];
-  }
-});
+      url : transcriptURL
+    });
+    creating.then(onCreated, onError);
 
+    if (active_callbacks.indexOf(tab.id) == -1)
+    {
+        active_callbacks.push(tab.id);
+        browser.tabs.executeScript(tab.id, { file: "/pluralsight-transcript.js" });
+    }
+}
+
+/* Register an event handler for the pageClick event to open a pop-up window
+   with the active course's transcript. */
 browser.pageAction.onClicked.addListener(showTranscript);
+/* Register event handler to new tab creation to add the page action button
+   whenever the user is at a pluralsight player page. */
+browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (tab.url.match(playerRegex)) {
+        browser.pageAction.show(tab.id);
+    }
+});
+/* Register event handler to capture injected client script communication back
+ to the background tab. */
+ browser.runtime.onConnect.addListener(function(p) { p.onMessage.addListener(updateTranscriptTime); });
